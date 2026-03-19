68173126c21bddbbc62f1838102ab21ab8f5ef13
Shreya2776 <saxenashreya315@gmail.com>
2026-03-20 02:58:49 +0530

    Google Auth setup

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: "http://localhost:5000/api/auth",
 });
-
+export default API;
 export const registerUser = (data) => API.post("/register", data);
 
 export const loginUser = (data) => API.post("/login", data);
